68e89831c8d6b4f0a395bd7edf305d2deb94ecdd
Mengxue1104 <59936757+Mengxue1104@users.noreply.github.com>
2022-03-11 01:52:32 -0700

    Update index.jsx

--- src/libs/firebase/index.jsx
+++ src/libs/firebase/index.jsx
@@ -6,12 +6,12 @@ import {getAuth} from "firebase/auth"
 // Set Up the Firebase Config with .env the Create React App Way.
 // This will have to be added to netlify.
 const firebaseConfig = { 
-  apiKey: process.env.REACT_APP_2008A2_FIREBASE_API_KEY ,
-  authDomain: process.env.REACT_APP_2008A2_FIREBASE_AUTH_DOMAIN ,
-  databaseURL: process.env.REACT_APP_2008A2_FIREBASE_DATABASE_URL ,
-  projectId: process.env.REACT_APP_2008A2_FIREBASE_PROJECT_ID ,
-  storageBucket: process.env.REACT_APP_2008A2_FIREBASE_STORAGE_BUCKET ,
-  messagingSenderId: process.env.REACT_APP_2008A2_FIREBASE_MESSAGING_SENDER_ID ,
+  apiKey: process.env.REACT_APP_2008A2_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_2008A2_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_2008A2_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_2008A2_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_2008A2_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_2008A2_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_2008A2_FIREBASE_APP_ID 
 };
 
