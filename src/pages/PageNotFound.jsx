f2c96a4dbd69491ebf207f63da4cfde17dac5c21
Mengxue Chen <59936757+Mengxue1104@users.noreply.github.com>
2022-03-11 11:27:44 -0700

    Update PageNotFound.jsx

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -7,7 +7,6 @@ export function PageNotFound(props) {
             <Grid container component="main" sx={{ height: '100vh' }} alignItems="center" justifyContent="center">
                 <div class="number">404</div>
                 <div class="text"><span>Ooops...</span><br />page not found</div>
-                <a class="me" href="https://codepen.io/uzcho_/pens/popular/?grid_type=list" target="_blank" rel="noreferrer"> </a>
             </Grid>
         </>
     )
