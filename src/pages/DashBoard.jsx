f63f7960a50a39f43087c645c0b973bb5d5be895
Mengxue1104 <59936757+Mengxue1104@users.noreply.github.com>
2022-03-21 17:50:42 -0600

    go back

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,31 +1,13 @@
-import React, {useState} from 'react';
+import * as React from 'react';
 
 import { Layout } from '../components/Layout';
-import { onAuthStateChanged } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
-import { auth } from 'libs/firebase' 
 
 export function DashBoard() {
     const content = <p>DashBoard Contents</p>
-    const [isUser, setIsUser] = useState(false)
-    const navigator = useNavigate();
-        onAuthStateChanged(auth, (user) => {
-            if(user){
-                setIsUser(true)
-            }else {
-                setIsUser(false)
-                //login page
-                navigator('/')
-            }
-        })
-        if(isUser){
-            return (
-                <Layout 
-                    title='Moxie Print Dashboard'
-                    content={content}
-                />
-            )
-        } else {
-            return null
-        }
+    return (
+        <Layout 
+            title='Moxie Print Dashboard'
+            content={content}
+        />
+    )
 }
